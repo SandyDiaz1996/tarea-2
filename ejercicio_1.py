7238a7d537a96832ca18eefc72a09e188f09fabd
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 14:44:02 -0500

    Ejercicio 3 creo clase Sintaxis

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -13,10 +13,25 @@
 # mensaje("Mi segundo programa. ")
 
 
-class Sintaxis:
-    def usoVariables(self):
-        edad, _peso = 50, 70.50
-        print("Edad: {}\nPeso: {}".format(edad, _peso))
+# class Sintaxis:
+#     def usoVariables(self):
+#         edad, _peso = 50, 70.50
+#         print("Edad: {}\nPeso: {}".format(edad, _peso))
 
-ejercicio1 = Sintaxis()
-ejercicio1.usoVariables()
+# ejercicio1 = Sintaxis()
+# ejercicio1.usoVariables()
+
+
+class Sintaxis:
+    instancia = 0
+    def __init__(self, dato = "Iniciando..."):
+        self.frase = dato
+
+    def usoVariables(self):
+        edad, _peso = 25, 63.4
+        nombre = "Sandy Diaz"
+        tipoSexo = "Femenino"
+        civilSoltera = False
+        print("Nombre: {}\nEdad: {}".format(nombre, edad))
+        print("Tipo de Sexo: {}\nEstado Civil Soltera: {}".format(tipoSexo, civilSoltera))
+        print("Peso: {}".format(_peso))
