7595fe399cf57cc33d13cad9c1df2399ef09c2ea
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 14:46:42 -0500

    Ejercicio 3 presentación de datos

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -28,10 +28,14 @@ class Sintaxis:
         self.frase = dato
 
     def usoVariables(self):
-        edad, _peso = 25, 63.4
+        edad, _peso = 25, 57.8
         nombre = "Sandy Diaz"
         tipoSexo = "Femenino"
         civilSoltera = False
         print("Nombre: {}\nEdad: {}".format(nombre, edad))
         print("Tipo de Sexo: {}\nEstado Civil Soltera: {}".format(tipoSexo, civilSoltera))
-        print("Peso: {}".format(_peso))
+        print("Peso: {}Kg".format(_peso))
+
+ejercicio1 = Sintaxis()
+print(ejercicio1.frase)
+ejercicio1.usoVariables()
