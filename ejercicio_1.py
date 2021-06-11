477eaa2411bda75a66186fa30ffcc0569caa3727
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 14:29:44 -0500

    Ejercicio 2 creación de clase Sintaxis

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,13 +1,19 @@
-numero = 50
-if type(numero) == int:
-    print("Respuesta:", numero * 2)
-else:
-    print("Este dato no es numérico.")
+# numero = 50
+# if type(numero) == int:
+#     print("Respuesta:", numero * 2)
+# else:
+#     print("Este dato no es numérico.")
 
-def mensaje(hombre):
-    print(hombre)
+# def mensaje(hombre):
+#     print(hombre)
 
 
-mensaje("")
-mensaje("Mi primer programa. ")
-mensaje("Mi segundo programa. ")
+# mensaje("")
+# mensaje("Mi primer programa. ")
+# mensaje("Mi segundo programa. ")
+
+
+class Sintaxis:
+    def usoVariables(self):
+        edad, _peso = 50, 70.50
+        print(edad, _peso)
