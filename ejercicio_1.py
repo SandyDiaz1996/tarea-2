efab701c2f7d9cd6a03c6d09de40fd5fa124ed75
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 14:23:40 -0500

    Funcion mi primer programa + saber si un número es entero

---
+++ ejercicio_1.py
@@ -0,0 +1,13 @@
+numero = 50
+if type(numero) == int:
+    print("Respuesta:", numero * 2)
+else:
+    print("Este dato no es numérico.")
+
+def mensaje(hombre):
+    print(hombre)
+
+
+mensaje("")
+mensaje("Mi primer programa. ")
+mensaje("Mi segundo programa. ")
