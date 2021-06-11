576b4f7b02dc9eab0e0781641e4af655c66ec99a
SandyDiaz1996 <sdiazo@unemi.edu.ec>
2021-06-11 15:24:55 -0500

    Ejercicio 4 creo clase y métodos

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -22,6 +22,25 @@
 # ejercicio1.usoVariables()
 
 
+# class Sintaxis:
+#     instancia = 0
+#     def __init__(self, dato = "Iniciando..."):
+#         self.frase = dato
+
+#     def usoVariables(self):
+#         edad, _peso = 25, 57.8
+#         nombre = "Sandy Diaz"
+#         tipoSexo = "Femenino"
+#         civilSoltera = False
+#         print("Nombre: {}\nEdad: {}".format(nombre, edad))
+#         print("Tipo de Sexo: {}\nEstado Civil Soltera: {}".format(tipoSexo, civilSoltera))
+#         print("Peso: {}Kg".format(_peso))
+
+# ejercicio1 = Sintaxis()
+# print(ejercicio1.frase)
+# ejercicio1.usoVariables()
+
+
 class Sintaxis:
     instancia = 0
     def __init__(self, dato = "Iniciando..."):
@@ -32,9 +51,30 @@ class Sintaxis:
         nombre = "Sandy Diaz"
         tipoSexo = "Femenino"
         civilSoltera = False
-        print("Nombre: {}\nEdad: {}".format(nombre, edad))
-        print("Tipo de Sexo: {}\nEstado Civil Soltera: {}".format(tipoSexo, civilSoltera))
-        print("Peso: {}Kg".format(_peso))
+        # print("Nombre: {}\nEdad: {}".format(nombre, edad))
+        # print("Tipo de Sexo: {}\nEstado Civil Soltera: {}".format(tipoSexo, civilSoltera))
+        # print("Peso: {}Kg".format(_peso))
+        
+        #TUPLA
+        usuario = ()
+        usuario = ("Sandy1996", 5648, "sdiazo@unemi.edu.ec", False)
+        
+        # LISTA
+        materias = []
+        materias = ["Programacion Web", "PHP", "Angular"]
+        materias[1] = "Python"
+        materias.append("Go")
 
-ejercicio1 = Sintaxis()
-ejercicio1.usoVariables()
+        # DICCIONARIO
+        docente = {}
+        docente = {"Nombre": "Esther", "Edad": 30, "Fac.": "FACI"}
+        docente["Carrera"] = "CS"
+        
+        # FORMAT
+        # print("""Mi nombre es {}, tengo {}
+        #          años""".format(nombre, edad))
+
+        print(usuario, materias, docente)
+        print(usuario, usuario[0], usuario[0:2], usuario[-1])
+        print(materias, materias[2:], materias[:1], materias[::], materias[-2:])
+        print(docente, docente["Nombre"])
